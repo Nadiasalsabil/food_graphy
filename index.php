c71c4bf059e7486f602a1c7d7ce563997bf793f2
Nadia Salsabil <salsabilnadia7@gmail.com>
2019-03-18 08:29:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Jammingup Studio | Snack</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/rangeslider.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+    <header class="site-navbar py-2 bg-white" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-11 col-xl-2">
+            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0"><img src="images/logo.png" width="140%"></a></h1>
+          </div>
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li class="active"><a href="index.php"><span>Home</span></a></li>
+                <li><a href="listings.html"><span>Produk</span></a></li>
+                <!-- <li class="has-children">
+                  <a href="about.html"><span>About</span></a>
+                  <ul class="dropdown">
+                    <li><a href="#">The Company</a></li>
+                    <li><a href="#">The Leadership</a></li>
+                    <li><a href="#">Philosophy</a></li>
+                    <li><a href="#">Careers</a></li>
+                  </ul>
+                </li> -->
+                <li><a href="blog.html"><span>Galery</span></a></li>
+                <li><a href="contact.html"><span>About</span></a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+
+        </div>
+      </div>
+      
+    </header>
+
+  
+
+    <div class="site-blocks-cover overlay" style="background-image: url(images/bg.png);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-10">
+            
+            
+            <div class="row justify-content-center mb-4">
+              <div class="col-md-8 text-center">
+                <h1 data-aos="fade-up">Jammingup Studio <br> <span class="typed-words"></span></h1>
+                <p data-aos="fade-up" data-aos-delay="100">Kapanpun dan dalam kesempatan apapun anda butuh snack, kami siap melayani order sekarang!</p>
+              </div>
+            </div>
+               
+ 
+                </div>
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>  
+
+
+    
+    <div class="site-section" data-aos="fade">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Product Trending</h2>
+            <p class="color-black-opacity-5">Produk yang paling sering dibeli dan dicari</p>
+          </div>
+        </div>
+
+        <div class="row">
+
+        <table cellspacing="0" cellpadding="0">
+  <tr>
+    <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+      
+              <div class="listing-image">
+                <img src="images/s1.jpg" alt="Image" class="img-fluid">
+              </div>
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s2.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s3.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s3.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+      <td> <div class="thumbnail">
+        <a href="images/s4.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s4.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+       <td> <div class="thumbnail">
+        <a href="images/s2.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s2.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+  </tr>
+  <tr>
+    <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+      
+              <div class="listing-image">
+                <img src="images/s1.jpg" alt="Image" class="img-fluid">
+              </div>
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s2.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s3.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s3.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s4.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s4.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+       <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s1.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+  </tr>
+</table>
+        
+      </div>
+  </div>
+</div>
+
+    
+
+    
+    <div class="site-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Discover Us</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-9">
+
+          <table border="0" cellspacing="7">
+          <tr>
+    <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+      
+              <div class="listing-image">
+                <img src="images/s1.jpg" alt="Image" class="img-fluid">
+              </div>
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s2.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s3.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s3.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+      <td> <div class="thumbnail">
+        <a href="images/s4.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s4.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+      
+  </tr>
+  <tr>
+    <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+      
+              <div class="listing-image">
+                <img src="images/s1.jpg" alt="Image" class="img-fluid">
+              </div>
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s2.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s3.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s3.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s4.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s4.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+      
+  </tr>
+  <tr>
+    <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+      
+              <div class="listing-image">
+                <img src="images/s1.jpg" alt="Image" class="img-fluid">
+              </div>
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s1.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s2.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s3.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s3.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+      <td> <div class="thumbnail">
+        <a href="images/s4.jpg" target="_blank">
+              <div class="listing-image">
+                <img src="images/s4.jpg" alt="Image" class="img-fluid">
+              </div>
+          
+        </a>
+      </div></td> 
+
+      
+  </tr>
+</table>
+          </div>
+          <div class="col-sm-3">.col-sm-4</div>
+         
+        </div>
+
+
+
+     
+
+
+         
+          </div>
+        </div>
+      </div>
+    </div>
+      
+   
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Products</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Features</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Follow Us</h2>
+                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+            <form action="#" method="post">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-primary text-white" type="button" id="button-addon2">Send</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/rangeslider.min.js"></script>
+  
+
+  <script src="js/typed.js"></script>
+            <script>
+            var typed = new Typed('.typed-words', {
+            strings: ["Food Graphy"],
+            typeSpeed: 80,
+            backSpeed: 80,
+            backDelay: 4000,
+            startDelay: 1000,
+            loop: true,
+            showCursor: true
+            });
+            </script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
